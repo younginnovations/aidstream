64ed539bc926cd9d086323d49f7eaf586b4f33ff
kritinemkul <nemkulk@gmail.com>
2015-09-18 15:26:22 +0545

    add version v202

---
+++ app/Core/V202/Repositories/Organization/NameRepository.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Core\V202\Repositories\Organization;
+
+use App\Core\V201\Repositories\Organization\NameRepository as V201NameRepository;
+
+class NameRepository extends V201NameRepository
+{
+
+}
