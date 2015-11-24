fa7060ac7d95d9ba4d2c0f5bf61efae4c933281d
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2015-11-24 11:01:50 +0545

    setting up the front end tools

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aidstream</title>
 
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
