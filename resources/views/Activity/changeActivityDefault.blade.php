d0416653b51a761607f6829606aac91a79571793
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2016-03-03 14:48:48 +0545

    word 'Change' removed from changeActivityDefault.blade.php

--- resources/views/Activity/changeActivityDefault.blade.php
+++ resources/views/Activity/changeActivityDefault.blade.php
@@ -8,7 +8,7 @@
             @include('includes.side_bar_menu')
             <div class="col-xs-9 col-md-9 col-lg-9 content-wrapper">
                 @include('includes.response')
-                <div class="panel-content-heading panel-title-heading panel-change-activity-heading">Change Activity Default Values</div>
+                <div class="panel-content-heading panel-title-heading panel-change-activity-heading">Activity Default Values</div>
                 <div class="panel panel-default panel-create">
                     <div class="panel-body">
                         <div class="create-form change-activity-form">
