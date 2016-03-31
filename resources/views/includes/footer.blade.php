d1cc73aed98eedf32486a930f9a377a8a5db4ee0
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2016-03-31 18:04:56 +0545

    texts updated

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -12,13 +12,13 @@
             <div class="col-md-12">
                 <ul>
                     <li><a href="{{ url('/about') }}">About</a></li>
-                    <li><a href="{{ url('/who-is-using') }}">Who's using</a></li>
+                    <li><a href="{{ url('/who-is-using') }}">Who's Using It?</a></li>
+                    <li><a href="https://github.com/younginnovations/aidstream-new/wiki/User-Guide" target="_blank">User Guide</a></li>
                     <!--<li><a href="#">Snapshot</a></li>-->
                 </ul>
                 <ul>
                     @if(auth()->check())
                         <li><a href="{{ url((auth()->user()->role_id == 1 || auth()->user()->role_id == 2) ? config('app.admin_dashboard') : config('app.super_admin_dashboard'))}}">Go to Dashboard</a></li>
-                        <li><a href="https://github.com/younginnovations/aidstream-new/wiki/User-Guide" target="_blank">User Guide</a></li>
                     @else
                         <li><a href="{{ url('/auth/login') }}">Login</a></li>
                         <li><a href="{{ url('/auth/register') }}">Register</a></li>
