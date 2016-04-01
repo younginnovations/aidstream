0365be3f352df05847a9832a5554c9020605060d
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2016-04-01 16:54:48 +0545

    theming of reset password page

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,60 +1,160 @@
-@extends('app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <title>Aidstream - Forgot Password</title>
+    <link rel="shotcut icon" type="image/png" sizes="32*32" href="{{ asset('/images/favicon.png') }}"/>
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/flag-icon.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<span>
-								<ul>
-									@foreach ($errors->all() as $error)
-										<li>{{ $error }}</li>
-									@endforeach
-								</ul>
-							</span>
-						</div>
-					@endif
+    <!-- Fonts -->
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-						<input type="hidden" name="token" value="{{ $token }}">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+    @yield('head')
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">New Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-default navbar-static">
+        <div class="navbar-header">
+            <a href="{{ url('/') }}" class="navbar-brand">Aidstream</a>
+            <button type="button" class="navbar-toggle collapsed">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="bar1"></span>
+                <span class="bar2"></span>
+                <span class="bar3"></span>
+            </button>
+        </div>
+        <div class="navbar-collapse navbar-right">
+            <ul class="nav navbar-nav">
+                <li><a class="{{ Request::is('about') ? 'active' : '' }}" href="{{ url('/about') }}">About</a></li>
+                <li><a class="{{ Request::is('who-is-using') ? 'active' : '' }}" href="{{ url('/who-is-using') }}">Who's
+                        Using</a></li>
+                <li><a href="https://github.com/younginnovations/aidstream-new/wiki/User-Guide" target="_blank">User
+                        Guide</a></li>
+                <!--<li><a href="#">Snapshot</a></li>-->
+            </ul>
+            <div class="action-btn pull-left">
+                @if(auth()->check())
+                    <a href="{{ url((auth()->user()->role_id == 1 || auth()->user()->role_id == 2) ? config('app.admin_dashboard') : config('app.super_admin_dashboard'))}}"
+                       class="btn btn-primary">Go
+                        to Dashboard</a>
+                @else
+                    <a href="{{ url('/auth/login')}}" class="btn btn-primary">Login/Register</a>
+                @endif
+            </div>
+        </div>
+    </nav>
+</header>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
+<div class="login-wrapper">
+    {{--		    <div class="language-select-wrapper">
+                    <label for="" class="pull-left">Language</label>
+                <div class="language-selector pull-left">
+                    <span class="flag-wrapper"><span class="img-thumbnail flag flag-icon-background flag-icon-{{ config('app.locale') }}"></span></span>
+                    <span class="caret pull-right"></span>
+                </div>
+                        <ul class="language-select-wrap language-flag-wrap">
+                            @foreach(config('app.locales') as $key => $val)
+                            <li class="flag-wrapper" data-lang="{{ $key }}"><span class="img-thumbnail flag flag-icon-background flag-icon-{{ $key }}"></span><span class="language">{{ $val }}</span></li>
+                            @endforeach
+                        </ul>
+                </div>--}}
+    <div class="container-fluid login-container reset-container">
+        <div class="row">
+            <div class="col-lg-4 col-md-8 col-md-offset-2 form-body">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <img src="{{url('images/logo.png')}}" alt="">
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Reset Password
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+                        <div class="panel-title">Reset password</div>
+                        <p>This will reset your password and log you into AidStream.</p>
+                    </div>
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                <span>{{ session('status') }}</span>
+                            </div>
+                        @endif
+
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <span>
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </span>
+                            </div>
+                        @endif
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <input type="hidden" name="token" value="{{ $token }}">
+
+                            <div class="form-group">
+                                <label class="control-label">E-Mail Address</label>
+                                <div class="col-md-12">
+                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label">New Password</label>
+                                <div class="col-md-12">
+                                    <input type="password" class="form-control" name="password">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label">Confirm Password</label>
+                                <div class="col-md-12">
+                                    <input type="password" class="form-control" name="password_confirmation">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-primary btn-submit">
+                                        Reset Password
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-@endsection
+<!-- Scripts -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="{{url('/js/jquery.cookie.js')}}"></script>
+<script>
+    $(document).ready(function () {
+        function hamburgerMenu() {
+            $('.navbar-toggle.collapsed').click(function () {
+                $('.navbar-collapse').toggleClass('out');
+                $(this).toggleClass('collapsed');
+            });
+        }
+
+        hamburgerMenu();
+    });
+</script>
+</body>
+</html>
+
+{{--l;fkmdhl;'sktrl';ku'e5--}}
