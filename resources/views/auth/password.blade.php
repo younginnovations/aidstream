b6cb853dc120773176e0cdcb94ac0738a956c53b
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2016-04-01 15:25:22 +0545

    password reset message fixed

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -86,14 +86,13 @@
 
                         @if (count($errors) > 0)
                             <div class="alert alert-danger">
-										<span>
-											<strong>Whoops!</strong> There were some problems with your input.
-											<ul>
-                                                @foreach ($errors->all() as $error)
-                                                    <li>{{ $error }}</li>
-                                                @endforeach
-                                            </ul>
-										</span>
+                                <span>
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </span>
                             </div>
                         @endif
 
