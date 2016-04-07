259607bb5ee139197401cece81783633e87606a3
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2016-04-07 11:35:58 +0545

    ui fixes

--- resources/views/Activity/upload.blade.php
+++ resources/views/Activity/upload.blade.php
@@ -7,7 +7,7 @@
     <div class="container main-container">
         <div class="row">
             @include('includes.side_bar_menu')
-            <div class="col-xs-9 col-md-9 col-lg-9 content-wrapper">
+            <div class="col-xs-9 col-md-9 col-lg-9 content-wrapper upload-activity-wrapper">
                 @include('includes.response')
                 <div class="element-panel-heading">
                     <div>
