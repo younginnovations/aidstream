4ad99e508aade80f36b9bb29d567942ee519f5dc
neha.rajbanshi <neha.rajbanshi@yipl.com.np>
2016-04-26 10:24:11 +0545

    AS-349 : Twitter publish for organization published files  -[x] added twitter class under services

---
+++ app/Services/Twitter/TwitterAPI.php
@@ -0,0 +1,42 @@
+<?php namespace App\Services\Twitter;
+
+use Thujohn\Twitter\Facades\Twitter;
+use Illuminate\Contracts\Logging\Log as Logger;
+
+class TwitterAPI
+{
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function post($settings, $org)
+    {
+        $apiId   = $settings['registry_info'][0]['publisher_id'];
+        $twitter = "";
+        if ($org->twitter != "") {
+            $twitter = $org->twitter;
+            if (substr($twitter, 0, 1) != '@') {
+                $twitter = ' @' . $twitter;
+            }
+        }
+
+        $status = $org->name . $twitter . " has published their #IATIData. View the data here: ";
+        $status .= 'http://iatiregistry.org/publisher/' . $apiId . ' #AidStream';
+
+        try {
+            $twitterResponse = Twitter::postTweet(['status' => $status, 'format' => 'json']);
+            $this->logger->info(
+                sprintf('Twitter has been successfully publish for %s with info : %s', $org->name, $twitterResponse)
+            );
+
+        } catch (\Exception $e) {
+            $this->logger->error(
+                sprintf('Twitter couldn\'t be tweeted for organization %s due to %s', $org->name, $e->getMessage()),
+                [
+                    'trace' => $e->getTraceAsString()
+                ]
+            );
+        }
+    }
+}
