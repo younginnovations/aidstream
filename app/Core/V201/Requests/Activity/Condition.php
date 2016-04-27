7b66c8424feddb2fb8d85e42673c5abd09e4be4c
Lunek Suwal <lunek.suwal@yipl.com.np>
2016-04-27 14:37:05 +0545

    Condition narrative fixes - [x] Fixes on form and validation - [x] Fixes in XML generation

--- app/Core/V201/Requests/Activity/Condition.php
+++ app/Core/V201/Requests/Activity/Condition.php
@@ -33,11 +33,15 @@ class Condition extends ActivityBaseRequest
         foreach ($formFields as $conditionIndex => $condition) {
             $conditionForm                                       = sprintf('condition.%s', $conditionIndex);
             $rules['condition_attached']                         = 'required';
-            $rules[sprintf('%s.condition_type', $conditionForm)] = 'required';
+            $rules[sprintf('%s.condition_type', $conditionForm)] = 'required_if:condition_attached,1';
             $rules                                               = array_merge(
                 $rules,
                 $this->getRulesForNarrative($condition['narrative'], $conditionForm)
             );
+
+            foreach ($condition['narrative'] as $narrativeIndex => $narrative) {
+                $rules[sprintf('%s.narrative.%s.narrative', $conditionForm, $narrativeIndex)][] = 'required_if:condition_attached,1';
+            }
         }
 
         return $rules;
@@ -52,13 +56,17 @@ class Condition extends ActivityBaseRequest
         $messages = [];
 
         foreach ($formFields as $conditionIndex => $condition) {
-            $conditionForm                                                   = sprintf('condition.%s', $conditionIndex);
-            $messages['condition_attached.required']                         = 'Condition Attached is Required';
-            $messages[sprintf('%s.condition_type.required', $conditionForm)] = 'Condition Type is Required';
-            $messages                                                        = array_merge(
+            $conditionForm                                                      = sprintf('condition.%s', $conditionIndex);
+            $messages['condition_attached.required']                            = 'Condition Attached is required';
+            $messages[sprintf('%s.condition_type.required_if', $conditionForm)] = 'Condition Type is required if Condition Attached is Yes.';
+            $messages                                                           = array_merge(
                 $messages,
                 $this->getMessagesForNarrative($condition['narrative'], $conditionForm)
             );
+
+            foreach ($condition['narrative'] as $narrativeIndex => $narrative) {
+                $messages[sprintf('%s.narrative.%s.narrative.required_if', $conditionForm, $narrativeIndex)] = 'Narrative is required if Condition Attached is Yes.';
+            }
         }
 
         return $messages;
