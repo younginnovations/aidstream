604bdee7abc3d9265fd5304f7763b5c1acd4b274
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2017-04-27 17:43:56 +0545

    app url added for env

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => env('APP_URL'),
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
