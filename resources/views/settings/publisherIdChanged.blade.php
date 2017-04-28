8be522293318c426a431898b6f4b685920779e9f
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2017-04-28 15:01:03 +0545

    Theming of published id modal

--- resources/views/settings/publisherIdChanged.blade.php
+++ resources/views/settings/publisherIdChanged.blade.php
@@ -8,12 +8,12 @@
         </div>
         <div class="modal-body">
             @if($isCorrect)
-                <p>You are about to change your publisher id from <strong>{{$oldPublisherId}}</strong> to <strong>{{$publisherId}}</strong></p>
-                <p>The following changes will come into effect after confirmation:</p>
+                <p>You are about to change your Publisher Id from <strong>{{$oldPublisherId}}</strong> to <strong>{{$publisherId}}</strong></p>
+                <p>The following changes will come into effect after your confirmation:</p>
                 <ul>
                     <li>Your present file(s) will be renamed.</li>
                     @if($inputApiKey)
-                        <li>Your current files will be deleted from IATI Registry and the renamed files will be published.</li>
+                        <li>Your current file(s) will be deleted from IATI Registry and the renamed files will be published.</li>
                     @endif
                 </ul>
                 @if(count(getVal($changes,['organizationData'],[])) > 0)
@@ -59,7 +59,7 @@
                 @endif
                 <div>
                     @if($inputApiKey)
-                        <p>Please enter your api key to reflect changes in registry.</p>
+                        <p>Please enter the API Key for the new Publisher Id for the changes to come into effect.</p>
                         {!! Form::label('apiKey', 'Api Key') !!}
                         {!! Form::text('apiKey', '',['id' => 'newApiKey']) !!}
                         <span id="apiStatus"></span>
@@ -69,21 +69,21 @@
                 </div>
             @elseif(!$isUnique)
                 <p>
-                    The publisher id <strong>{{$publisherId}}</strong> has already been used. Please check your publisher id and try again.
+                    The Publisher Id <strong>{{$publisherId}}</strong> is already being used. Please check your Publisher Id and try again.
                 </p>
             @elseif(!$isAuthorized)
                 <p>
-                    You are not authorized to change the settings. Please contact your administrator to change the settings.
+                    You are not authorized to change the Settings. Please contact your administrator to change the Settings.
                 </p>
             @else
                 <p>
-                    The publisher id you have entered is incorrect. Since, these changes will affect your datasets and xml files, we cannot allow you to make these changes.
+                    The Publisher Id you have entered is incorrect. Since, these changes will affect your datasets and xml files, we cannot allow you to make these changes.
                 </p>
             @endif
         </div>
         <div class="modal-footer">
             @if($isCorrect)
-                <button type="submit" class="btn btn-primary" id="saveChanges" @if($inputApiKey) disabled @endif>Yes</button>
+                <button type="submit" class="btn btn-primary" id="saveChanges" {{ !$inputApiKey ?: 'disabled' }}>Yes</button>
             @endif
             <button type="button" class="btn btn-default" onclick="window.location.href= '/publishing-settings'">Close</button>
         </div>
