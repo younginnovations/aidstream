84f9703bb252b12ea497131d1882aa0cf9d44906
Bibek M Acharya <bibek.acharya@yipl.com.np>
2018-12-04 15:20:54 +0545

    Aidstream NP Authentication

--- app/Http/routes/Np/municipalityAdmin.php
+++ app/Http/routes/Np/municipalityAdmin.php
@@ -4,6 +4,13 @@ $router->group(
     ['namespace' => 'MunicipalityAdmin', 'MunicipalityAdmin' => true],
     function ($router) {
         $router->get(
+            'municipality/admin/dashboard',
+            [
+                'as'   => 'municipalityAdmin.dashboard',
+                'uses' => 'OrganizationController@dashboard'
+            ]
+        );
+        $router->get(
             'municipality/admin/list-organization',
             [
                 'as'   => 'municipalityAdmin.list-organization',
@@ -25,122 +32,17 @@ $router->group(
             ]
         );
         $router->get(
-            'admin/list-organization/lite-demo',
+            'municipality/admin/masquerade-organization/{orgId?}/user/{userId?}',
             [
-                'as'   => 'admin.list-organization.tz',
-                'uses' => 'OrganizationController@listOrganizations'
-            ]
-        );
-        $router->get(
-            'admin/add-organization',
-            [
-                'as'   => 'admin.add-organization',
-                'uses' => 'OrganizationController@add'
-            ]
-        );
-        $router->post(
-            'admin/add-organization',
-            [
-                'as'   => 'admin.add-organization',
-                'uses' => 'OrganizationController@save'
-            ]
-        );
-        $router->get(
-            'admin/edit-organization/{id}',
-            [
-                'as'   => 'admin.edit-organization',
-                'uses' => 'OrganizationController@add'
-            ]
-        );
-        $router->put(
-            'admin/edit-organization/{id}',
-            [
-                'as'   => 'admin.edit-organization',
-                'uses' => 'OrganizationController@save'
-            ]
-        );
-        $router->get(
-            'admin/change-organization-status/org_id/{id}/status/{status}',
-            [
-                'as'   => 'admin.change-organization-status',
-                'uses' => 'OrganizationController@changeOrganizationStatus'
-            ]
-        );
-        $router->get(
-            'admin/delete-organization/{id}',
-            [
-                'as'   => 'admin.delete-organization',
-                'uses' => 'OrganizationController@deleteOrganization'
-            ]
-        );
-        $router->get(
-            'admin/masquerade-organization/{orgId?}/user/{userId?}',
-            [
-                'as'   => 'admin.masquerade-organization',
+                'as'   => 'municipalityAdmin.masquerade-organization',
                 'uses' => 'OrganizationController@masqueradeOrganization'
             ]
         );
         $router->get(
-            'admin/switch-back',
+            'municipality/admin/switch-back',
             [
-                'as'   => 'admin.switch-back',
-                'uses' => 'OrganizationController@switchBackAsSuperAdmin'
-            ]
-        );
-        $router->get(
-            'admin/group-organizations',
-            [
-                'as'   => 'admin.group-organizations',
-                'uses' => 'OrganizationGroupController@lists'
-            ]
-        );
-        $router->get(
-            'admin/create-organization-group',
-            [
-                'as'   => 'admin.create-organization-group',
-                'uses' => 'OrganizationGroupController@create'
-            ]
-        );
-        $router->post(
-            'admin/create-organization-group',
-            [
-                'as'   => 'admin.create-organization-group',
-                'uses' => 'OrganizationGroupController@save'
-            ]
-        );
-        $router->get(
-            'admin/edit-group/{id}',
-            [
-                'as'   => 'admin.edit-group',
-                'uses' => 'OrganizationGroupController@create'
-            ]
-        );
-        $router->put(
-            'admin/edit-group/{id}',
-            [
-                'as'   => 'admin.edit-group',
-                'uses' => 'OrganizationGroupController@save'
-            ]
-        );
-        $router->get(
-            'admin/hide-organization/org_id/{id}/status/{status}',
-            [
-                'as'   => 'admin.hide-organization',
-                'uses' => 'OrganizationController@hideOrganization'
-            ]
-        );
-        $router->get(
-            'admin/exportOrganizationInfo',
-            [
-                'as'   => 'admin.exportOrganizationInfo',
-                'uses' => 'OrganizationController@exportOrganizationInfo'
-            ]
-        );
-        $router->post(
-            'admin/change/system_version/{orgId}',
-            [
-                'as'   => 'admin.change.system_version',
-                'uses' => 'OrganizationController@changeSystemVersion'
+                'as'   => 'municipalityAdmin.switch-back',
+                'uses' => 'OrganizationController@switchBackAsMunicipalityAdmin'
             ]
         );
         $router->get(
